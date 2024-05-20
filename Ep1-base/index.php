f9e258d37dd1f67cf788db1db85e7523ce911476
BuildTools <unconfigured@null.spigotmc.org>
2024-05-20 14:54:42 +0200

    Episode 1

---
+++ Ep1-base/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$world = "World";
+
+$tableau = ["0", 1, 2, "Hello World"];
+
+echo "Hello " . $world . $tableau[0];
+
+var_dump($tableau);
+
+echo $_GET["bonjour"];
+
+echo $_SERVER["SERVER_NAME"];
+
+
+?>
